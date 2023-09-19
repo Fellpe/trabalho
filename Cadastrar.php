520283574cba0278aabe7ffa13052aa22f4aad9a
Fellpe <58045147+Fellpe@users.noreply.github.com>
2023-09-19 11:51:00 -0300

    Update Cadastrar.php

--- Cadastrar.php
+++ Cadastrar.php
@@ -4,8 +4,8 @@ require "Casa.php";
 require "RepositorioCasa.php";
 $repCasa = new RepositorioCasa();
 
-$Casa1 = new Casa("Rua Wilson Costa","20m²","4","100.000,00","1995");
-$Casa2 = new Casa("Rua Doutor Albert Sabin","40m²","3","130.000,00","2000");
+$Casa1 = new Casa("Rua Wilson Costa","20m","4","100.000,00","1995");
+$Casa2 = new Casa("Rua Doutor Albert Sabin","40m","3","130.000,00","2000");
 
 $repCasa->cadastrar($banco,$Casa1);
 $repCasa->cadastrar($banco,$Casa2);
