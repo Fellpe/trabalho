361eb5b531f343539ff4991a8da58114d0924beb
Fellpe <58045147+Fellpe@users.noreply.github.com>
2023-09-19 11:49:15 -0300

    Update Listar.php

--- Listar.php
+++ Listar.php
@@ -5,14 +5,14 @@ require "RepositorioCasa.php";
 
 $repCasa = new RepositorioCasa();
 
-$todasCasas = $repCasa->exibirTudo($Casa);
+$todasCasas = $repCasa->exibirTudo($banco);
 
 foreach($todasCasas as $Casa){
     echo "\n -----------------------------------";
-    echo "\n Endereço | {$Casa['endereco']}";
-    echo "\n Tamanho      | {$Casa['tamanho']}";
+    echo "\n Endereço               | {$Casa['endereco']}";
+    echo "\n Tamanho                | {$Casa['tamanho']}";
     echo "\n Numero de quartos      | {$Casa['numeroquartos']}";
-    echo "\n Valor        | {$Casa['valor']}";
-    echo "\n Ano de construção        | {$Casa['anoconst']}";
+    echo "\n Valor                  | {$Casa['valor']}";
+    echo "\n Ano de construção      | {$Casa['anoconst']}";
     echo "\n -----------------------------------";
 }
