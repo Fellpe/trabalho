1084917c7dad55caf4f1c5a0e48026cea1528115
Fellpe <58045147+Fellpe@users.noreply.github.com>
2023-09-22 06:11:06 -0300

    Update RepositorioCasa.php

--- RepositorioCasa.php
+++ RepositorioCasa.php
@@ -4,6 +4,7 @@ class RepositorioCasa
 {
     public function cadastrar(PDO $banco, Casa $Casa)
     {
+        //Aqui prepara a instrução SQL para criar o registro na tabela
         $sqlInsert = "INSERT INTO CASA(endereco,tamanho,numeroquartos,valor,anoconst) VALUES (:e,:t,:nq,:v,:ac)";
 
         $insert = $banco->prepare($sqlInsert);
@@ -13,19 +14,20 @@ class RepositorioCasa
         $numeroquartos = $Casa->exibirNumeroQuartos();
         $valor = $Casa->exibirValor();
         $anoconst = $Casa->exibirAnoConstrucao();
-
+        //definindo o valor dos parâmetros 
         $insert->bindParam(":e", $endereco);
         $insert->bindParam(":t", $tamanho);
         $insert->bindParam(":nq", $numeroquartos);
         $insert->bindParam(":v", $valor);
         $insert->bindParam(":ac", $anoconst);
-
+        //executa o comando de insert no banco 
         $insert->execute();
     }
     
     public function exibirTudo(PDO $banco){ 
         $sql = "SELECT * FROM CASA";
-        $dado = $banco->query($sql);
+        $dado = $banco->query($sql); //executa o SQL do banco
+        //estrutura os valores como obtidos pelo $select como um vetor indexado
         return $dado->fetchAll(PDO::FETCH_ASSOC);
     }
 }
